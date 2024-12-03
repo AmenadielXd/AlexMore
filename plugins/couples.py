3d8612e528a05c197a5fd0883911fc1e34eec3f8
AmenadielXd <rv1858000@gmail.com>
2024-12-03 08:35:12 +0530

    Update couples.py

--- plugins/couples.py
+++ plugins/couples.py
@@ -1,17 +1,22 @@
-import os 
 import random
-from datetime import datetime 
-from telegraph import upload_file
-from PIL import Image, ImageDraw
-from pyrogram import *
-from pyrogram.enums import *
+from datetime import datetime
 
-# BOT FILE NAME
-from Alex import app as app
+import pytz
+from pyrogram import enums, filters
 
+from Alex import app
+from Alex.core.decorators.errors import capture_err
+from wbb.utils.detabase import get_couple, save_couple
+
+# Date and time
 def dt():
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    # Set the timezone to Indian Standard Time
+    ist_timezone = pytz.timezone("Asia/Kolkata")
+
+    # Get the current time in IST
+    ist_now = datetime.now(ist_timezone)
+
+    dt_string = ist_now.strftime("%d/%m/%Y %H:%M")
     dt_list = dt_string.split(" ")
     return dt_list
 
@@ -26,94 +31,59 @@ def dt_tom():
     )
     return a
 
-tomorrow = str(dt_tom())
-today = str(dt()[0])
 
-@app.on_message(filters.command(["couples", "couple"], prefixes=["/", "!", ".", "@"]))
-async def ctest(_, message):
-    cid = message.chat.id
-    if message.chat.type == ChatType.PRIVATE:
-        await message.reply_text("⬤ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋs ɪɴ ɢʀᴏᴜᴘs.")
-        await message.delete()
-        return
-    
+def today():
+    return str(dt()[0])
+
+
+def tomorrow():
+    return str(dt_tom())
+
+
+@app.on_message(filters.command("detect_gay"))
+@capture_err
+async def couple(_, message):
+    if message.chat.type == enums.ChatType.PRIVATE:
+        return await message.reply_text("This command only works in groups.")
+
+    m = await message.reply("Detecting gay among us...")
+
     try:
-        msg = await message.reply_text("<b>ᴘʀᴏᴄᴇssɪɴɢ . . .</b>")
-        # GET LIST OF USERS
-        list_of_users = []
-
-        async for i in app.get_chat_members(message.chat.id, limit=50):
-            if not i.user.is_bot:
-                list_of_users.append(i.user.id)
-
-        c1_id = random.choice(list_of_users)
-        c2_id = random.choice(list_of_users)
-        while c1_id == c2_id:
+        chat_id = message.chat.id
+        is_selected = await get_couple(chat_id, today())
+        if not is_selected:
+            list_of_users = []
+            async for i in app.get_chat_members(message.chat.id):
+                if not i.user.is_bot and not i.user.is_deleted:
+                    user = await app.get_users(i.user.id)
+                    list_of_users.append(user.id)
+            if len(list_of_users) < 2:
+                return await m.edit("Not enough users")
             c1_id = random.choice(list_of_users)
+            c2_id = random.choice(list_of_users)
+            while c1_id == c2_id:
+                c1_id = random.choice(list_of_users)
+            c1_mention = (await app.get_users(c1_id)).mention
+            c2_mention = (await app.get_users(c2_id)).mention
 
-        photo1 = (await app.get_chat(c1_id)).photo
-        photo2 = (await app.get_chat(c2_id)).photo
+            couple_selection_message = f"""**Couple of the day:**
+{c1_mention} + {c2_mention} = ❤️
 
-        N1 = (await app.get_users(c1_id)).mention 
-        N2 = (await app.get_users(c2_id)).mention
+__New couple of the day may be chosen at 12AM {tomorrow()}__"""
+            await m.edit(couple_selection_message)
+            couple = {"c1_id": c1_id, "c2_id": c2_id}
+            await save_couple(chat_id, today(), couple)
 
-        try:
-            p1 = await app.download_media(photo1.big_file_id, file_name="pfp.png")
-        except Exception:
-            p1 = "Alex/assets/upic.png"
-        try:
-            p2 = await app.download_media(photo2.big_file_id, file_name="pfp1.png")
-        except Exception:
-            p2 = "Alex/assets/upic.png"
+        elif is_selected:
+            c1_id = int(is_selected["c1_id"])
+            c2_id = int(is_selected["c2_id"])
+            c1_name = (await app.get_users(c1_id)).first_name
+            c2_name = (await app.get_users(c2_id)).first_name
+            couple_selection_message = f"""Couple of the day:
+[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
 
-        img1 = Image.open(f"{p1}")
-        img2 = Image.open(f"{p2}")
-
-        img = Image.open("Alex/assets/COUPLES2.PNG")
-
-        img1 = img1.resize((595, 595))
-        img2 = img2.resize((595, 595))
-
-        mask = Image.new('L', img1.size, 0)
-        draw = ImageDraw.Draw(mask) 
-        draw.ellipse((0, 0) + img1.size, fill=290)
-
-        mask1 = Image.new('L', img2.size, 0)
-        draw = ImageDraw.Draw(mask1) 
-        draw.ellipse((0, 0) + img2.size, fill=290)
-
-        img1.putalpha(mask)
-        img2.putalpha(mask1)
-
-        draw = ImageDraw.Draw(img)
-
-        img.paste(img1, (96, 239), img1)
-        img.paste(img2, (1220, 239), img2)
-
-        img.save(f'test_{cid}.png')
-
-        TXT = f"""
-<blockquote>ㅤ   ◦•●◉✿ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ  ✿◉●•◦</blockquote>
-▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱
-
- {N1} + {N2} = ♥︎
-
-⬤ ɴᴇxᴛ ᴄᴏᴜᴘʟᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow}
-▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱
-"""
-
-        await message.reply_photo(f"test_{cid}.png", caption=TXT)
-        await msg.delete()
-        await message.delete()  # Delete user command message
-        a = upload_file(f"test_{cid}.png")
-        for x in a:
-            img = "https://graph.org/" + x
-
+__New couple of the day may be chosen at 12AM {tomorrow()}__"""
+            await m.edit(couple_selection_message)
     except Exception as e:
-        print(str(e))
-    try:
-        os.remove(f"./downloads/pfp1.png")
-        os.remove(f"./downloads/pfp2.png")
-        os.remove(f"test_{cid}.png")
-    except Exception:
-        pass
+        print(e)
+        await message.reply_text(e)
