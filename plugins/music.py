d531f59946ebc7f52d52cc7f033bd606a3c4c5c8
AmenadielXd <rv1858000@gmail.com>
2024-12-05 03:16:38 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -248,7 +248,7 @@ def fetch_song(song_name):
 async def handle_song(client, message):
     song_name = message.text.split(" ", 1)[1] if len(message.text.split(" ", 1)) > 1 else None
     if not song_name:
-        return await message.reply("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ sᴏɴɢ ɴᴀᴍᴇ ᴀғᴛᴇʀ ᴛʜᴇ /song ᴄᴏᴍᴍᴀɴᴅ..")
+        return await message.reply("<b>ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ sᴏɴɢ ɴᴀᴍᴇ . . .</b>")
 
     song_info = fetch_song(song_name)
     if not song_info:
@@ -263,7 +263,8 @@ async def handle_song(client, message):
             if chunk:
                 file.write(chunk)
 
-    caption = (f"""<b>⬤ sᴏɴɢ ɴᴀᴍᴇ ➠</b> {song_info['trackName']}\n\n<b>● ᴀʟʙᴜᴍ ➠</b> {song_info['album']}\n<b>● ʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ ➠</b> {song_info['releaseDate']}\n<b>● ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➠</b> {message.from_user.mention}""")
+    # Caption removed
+    caption = ""
 
     # Send audio and clean up
     await message.reply_audio(audio=open(filename, "rb"), caption=caption)
