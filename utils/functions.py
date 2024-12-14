8589378679f3e3b1484a20cfef2ab9da5373f3e4
AmenadielXd <rv1858000@gmail.com>
2024-12-14 09:46:27 +0530

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -351,3 +351,47 @@ async def get_user_id_and_usernames(client) -> dict:
         users_[user_id] = username
 
     return users_
+
+def generate_captcha():
+    # Generate one letter
+    def gen_letter():
+        return chr(randint(65, 90))
+
+    def rndColor():
+        return randint(64, 255), randint(64, 255), randint(64, 255)
+
+    def rndColor2():
+        return randint(32, 127), randint(32, 127), randint(32, 127)
+
+    # Generate a 4 letter word
+    def gen_wrong_answer():
+        word = ""
+        for _ in range(4):
+            word += gen_letter()
+        return word
+
+    # Generate 8 wrong captcha answers
+    wrong_answers = []
+    for _ in range(8):
+        wrong_answers.append(gen_wrong_answer())
+
+    width = 80 * 4
+    height = 100
+    correct_answer = ""
+    font = ImageFont.truetype("Alex/assets/arial.ttf", 55)
+    file = f"Alex/assets/{randint(1000, 9999)}.jpg"
+    image = Image.new("RGB", (width, height), (255, 255, 255))
+    draw = ImageDraw.Draw(image)
+
+    # Draw random points on image
+    for x in range(width):
+        for y in range(height):
+            draw.point((x, y), fill=rndColor())
+
+    for t in range(4):
+        letter = gen_letter()
+        correct_answer += letter
+        draw.text((60 * t + 50, 15), letter, font=font, fill=rndColor2())
+    image = image.filter(ImageFilter.BLUR)
+    image.save(file, "jpeg")
+    return [file, correct_answer, wrong_answers]
